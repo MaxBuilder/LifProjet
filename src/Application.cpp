66875d9cc83ab5b468afe12161028402ed954d50
Raphaël <mripadspace@hotmail.fr>
2021-01-26 10:37:17 +0100

    Added Apllication class

---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 33771 on 26/01/2021.
+//
+
+#include "Application.h"
---
+++ src/Application.h
@@ -0,0 +1,14 @@
+//
+// Created by Raph on 26/01/2021.
+//
+
+#ifndef LIFPROJET_APPLICATION_H
+#define LIFPROJET_APPLICATION_H
+
+
+class Application {
+
+};
+
+
+#endif //LIFPROJET_APPLICATION_H
